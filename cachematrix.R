2afa56a4d4fe36b6a1d8f3f31734fcc21b35b148
Anna Kubasiak <kubasiak.anna@gmail.com>
2015-04-26 20:58:35 +0200

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,5 @@
 makeCacheMatrix <-function(x=matrix()){
+#this function creates a generelized Vector which fourth value is getting inverse of matrix x
   im <- NULL
   set <- function(y) {
     x <<- y
@@ -15,6 +16,7 @@ makeCacheMatrix <-function(x=matrix()){
 
 
 cacheSolve<-function(x, ...){
+  #This function first checks if value of inverse msatrix is calculated and stored and then either returns it or calculates it and returns it
   im<-x$getinv()
   if(!is.null(im)) {
     message("getting cached data")
